9431bf8790e62b214e7f26c96bbb3eb6be1342a6
Viet Tran <viettran101294@gmail.com>
2023-04-17 20:02:04 -0400

    Add auto generated files with codegen

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import App from "./App";
 import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
+import { ApolloProvider } from "@apollo/client";
+import { apolloClient } from "./app/graphql";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ApolloProvider client={apolloClient}>
+        <App />
+      </ApolloProvider>
     </Provider>
   </React.StrictMode>
 );
